9eba182c804061658600a561a14845d387217f17
Jules Joseph <dev@josephtechnology.net>
2017-04-15 14:55:54 +0100

    Update export to use with when writing to file Swallow all exceptions for time being

--- export.py
+++ export.py
@@ -6,18 +6,19 @@ import sys
 
 
 def saveTweetJsonToFile(tweet):
-    fileName = os.path.join(sys.path[0],'export','tweet_data_{0}.json'.format(getDailyFileName()))
-    print(fileName)
+    try:
+        fileName = os.path.join(sys.path[0],'export','tweet_data_{0}.json'.format(getDailyFileName()))
+        print(fileName)
 
-    savefile = io.open(fileName, 'a', encoding='utf-8')
+        json_str = json.dumps(tweet._json)
+        json_str = json_str.replace('\"id_str\"','\"_id\"', 1)
 
-    json_str = json.dumps(tweet._json)
-    json_str = json_str.replace('\"id_str\"','\"_id\"', 1)
-    #print(json_str)
-
-    savefile.write(str(json_str))
-    savefile.write("\n")
-    savefile.close()
+        with open(fileName, 'a', encoding='utf-8') as jsonFile:
+            jsonFile.write(str(json_str))
+            jsonFile.write("\n")
+            jsonFile.close()
+    except Exception as e:
+        pass
 
 def getDailyFileName():
     ## dd/mm/yyyy format
