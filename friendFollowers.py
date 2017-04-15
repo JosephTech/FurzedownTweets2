3866bd2936354b81a980dcd08e8bceb722b9b008
Jules Joseph <dev@josephtechnology.net>
2017-04-15 16:21:36 +0100

    Removed commented out code

--- friendFollowers.py
+++ friendFollowers.py
@@ -5,13 +5,7 @@ import os
 def FollowBackNewFollowers(t, settings):
     settings = readConfig.ConfigSettings('config.json')
 
-    # t = twitter.Wrapper(access_token=settings.AccessToken,
-    #                         access_token_secret=settings.AccessTokenSecret,
-    #                         consumer_key=settings.ConsumerKey,
-    #                         consumer_secret=settings.ConsumerSecret)
-
     try:
-        #newFollowers = t.GetNewFollowers()
         newFollowersFollowed = t.BefriendNewFollowers(t.GetNewFollowers(), settings.NewFollowerMessage, settings.LoggingRecipient)
         return newFollowersFollowed
     except Exception as e:
