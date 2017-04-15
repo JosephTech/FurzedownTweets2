9a17c787851fad4bb43bf558a759c1662afe0ca1
Jules Joseph <dev@josephtechnology.net>
2017-04-15 14:43:40 +0100

    Module for exporting tweets as json to a daily file

--- export.py
+++ export.py
@@ -1,12 +1,24 @@
 import io
 import json
+import time
+import os
+import sys
 
 
 def saveTweetJsonToFile(tweet):
-    savefile = io.open('tweet_data.json', 'a', encoding='utf-8')
+    fileName = os.path.join(sys.path[0],'export','tweet_data_{0}.json'.format(getDailyFileName()))
+    print(fileName)
+
+    savefile = io.open(fileName, 'a', encoding='utf-8')
 
     json_str = json.dumps(tweet._json)
+    json_str = json_str.replace('\"id_str\"','\"_id\"', 1)
+    #print(json_str)
 
     savefile.write(str(json_str))
-    savefile.write(",\n")
+    savefile.write("\n")
     savefile.close()
+
+def getDailyFileName():
+    ## dd/mm/yyyy format
+    return time.strftime("%Y%m%d")
