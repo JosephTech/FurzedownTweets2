6139690af938537024908bc4b10d770707343c19
Jules Joseph <julian@josephtechnology.net>
2017-09-07 21:58:43 +0100

    Update readConfig.py

--- readConfig.py
+++ readConfig.py
@@ -57,6 +57,10 @@ class ConfigSettings:
     def ConfigPath(self):
         return os.path.join(os.getcwd(),'config')
 
+    @property
+    def ConnectionString(self):
+        return "mongodb://julesjoseph:ftsP93gnEqiEXZPe@cluster0-shard-00-00-rsihj.mongodb.net:27017,cluster0-shard-00-01-rsihj.mongodb.net:27017,cluster0-shard-00-02-rsihj.mongodb.net:27017/furzedowntweets?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+
     def getConfigFilePath(self, fileName):
         return os.path.join(self.ConfigPath, fileName)
 
