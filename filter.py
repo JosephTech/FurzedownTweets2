159f34be12e69dab292e315cd7a04203ec78104a
Jules Joseph <julian@josephtechnology.net>
2018-02-02 16:12:30 +0000

    Update filter.py
    Update text attribute to full_text now that we are using full_text mode

--- filter.py
+++ filter.py
@@ -1,15 +1,15 @@
 
 def FilterReplies(tweets):
-    return list(filter(lambda status: status.text[0] != "@", tweets))
+    return list(filter(lambda status: status.full_text[0] != "@", tweets))
 
 def FilterBannedWords(tweets, bannedWords):
-    return list(filter(lambda status: not any(word in status.text.split() for word in bannedWords), tweets))
+    return list(filter(lambda status: not any(word in status.full_text.split() for word in bannedWords), tweets))
 
 def FilterBannedUsers(tweets, bannedUsers):
     return list(filter(lambda status: status.author.screen_name not in bannedUsers, tweets))
 
 def FilterMultipleHashTags(tweets, maxHashTags):
-    return list(filter(lambda status: status.text.count('#') <= maxHashTags, tweets))
+    return list(filter(lambda status: status.full_text.count('#') <= maxHashTags, tweets))
 
 def FilterRetweets(tweets):
-    return list(filter(lambda status: status.text.count('RT') == 0, tweets))
+    return list(filter(lambda status: status.full_text.count('RT') == 0, tweets))
