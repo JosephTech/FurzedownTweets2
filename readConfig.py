8989ce338a0a78d29aec5f847a4d88640af66eff
Jules Joseph <dev@josephtechnology.net>
2018-02-06 14:08:23 +0000

    Removed connection string

--- readConfig.py
+++ readConfig.py
@@ -59,7 +59,7 @@ class ConfigSettings:
 
     @property
     def ConnectionString(self):
-        return "mongodb://julesjoseph:ftsP93gnEqiEXZPe@cluster0-shard-00-00-rsihj.mongodb.net:27017,cluster0-shard-00-01-rsihj.mongodb.net:27017,cluster0-shard-00-02-rsihj.mongodb.net:27017/furzedowntweets?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+		return self.getConfigSection('mongoDb')['connectionString']
 
     def getConfigFilePath(self, fileName):
         return os.path.join(self.ConfigPath, fileName)
